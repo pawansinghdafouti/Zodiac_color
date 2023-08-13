e6250d17cac3630ad5a794ec24a3d3191a5fc75e
pawan singh dafouti <114570692+pawansinghdafouti@users.noreply.github.com>
2023-08-13 12:19:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const zodiac = document.getElementById("zodiac");
+
+const body = document.body;
+
+const changeBackground = () => {
+  
+    if (zodiac.value == "Aries") {
+        body.style.backgroundColor="orange";
+    }
+    if (zodiac.value == "Tarus") {
+        body.style.backgroundColor="pink";
+    }
+    if (zodiac.value == "Gemini") {
+        body.style.backgroundColor="#00A97F";
+    }
+    if (zodiac.value == "Cancer") {
+        body.style.backgroundColor="gold";
+    }
+    if (zodiac.value == "Leo") {
+        body.style.backgroundColor="mediumslateblue";
+    }
+    if (zodiac.value == "Virgo") {
+        body.style.backgroundColor="yellow";
+    }
+    if (zodiac.value == "Libra") {
+        body.style.backgroundColor="lavender";
+    }
+    if (zodiac.value == "Scorpion") {
+        body.style.backgroundColor="black";
+    }
+    if (zodiac.value == "Sahittarius") {
+        body.style.backgroundColor="blue";
+    }
+    if (zodiac.value == "Aquarius") {
+        body.style.backgroundColor="orange";
+    }
+    if (zodiac.value == "Capricorn") {
+        body.style.backgroundColor="gray";
+    }
+    if (zodiac.value == "CapriPiscescorn") {
+        body.style.backgroundColor="red";
+    }
+};
